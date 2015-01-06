67351ebc0e2f81e0f827b836fa6ea27b96bcbb3a
Gleb Pospelov <codefo@codefo.com>
2015-01-06 04:31:47 +0300

    Implemented reading code from file and stdin

---
+++ src/brainfuck.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "brainfuck.h"
+
+void run(char *source) {
+    puts(source);
+}
---
+++ src/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "brainfuck.h"
+
+#define PROGRAM_NAME "cfuck"
+#define VERSION "0.1.0"
+#define YEAR "2015"
+#define AUTHOR "Gleb Pospelov <codefo@codefo.com>"
+
+#define ERROR_FILE_NOT_OPENED "Input file not opened"
+
+void show_prompt() {
+    printf("%s %s %s\n", PROGRAM_NAME, VERSION, YEAR);
+    printf("%s\n\n", AUTHOR);
+}
+
+void run_file(char *path) {
+    char *buffer = NULL;
+    FILE *file = NULL;
+    size_t length;
+
+    file = fopen(path, "rb");
+
+    if (file == NULL) {
+        fputs(ERROR_FILE_NOT_OPENED, stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(file, 0, SEEK_END);
+    length = (size_t) ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    buffer = malloc(length);
+
+    if (buffer) {
+        fread(buffer, 1, length, file);
+    }
+
+    fclose(file);
+
+    run(buffer);
+}
+
+void run_repl() {
+    unsigned long index = 1;
+    char *line = NULL;
+    size_t length = 0;
+
+    while(index)
+    {
+        printf("%lu> ", index);
+
+        if (feof(stdin) != 0) break;
+
+        getline(&line, &length, stdin);
+        run(line);
+
+        free(line);
+        line = NULL;
+
+        index++;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 1) {
+        run_file(argv[1]);
+    } else {
+        show_prompt();
+        run_repl();
+    }
+
+    return EXIT_SUCCESS;
+}
+
